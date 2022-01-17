cc79879cf73ef973130bdb3ff86eefa0b43b7386
Jonathan Marrujo <jonmarrujo@gmail.com>
2022-01-17 09:57:47 -0800

    Restarted database from scratch for articles

--- db/migrate/20220115152338_create_articles.rb
+++ db/migrate/20220115152338_create_articles.rb
@@ -1,8 +1,10 @@
-class CreatArticles < ActiveRecord::Migration[6.1]
+class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
       t.string :title
       t.text :description
+
+      t.timestamps
     end
   end
 end
